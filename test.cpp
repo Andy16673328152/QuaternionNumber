8d7b41740588ab643c24755fb4dfd63a1bba53ef
Andy16673328152 <1435417173@qq.com>
2026-05-27 14:20:52 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 #include <cassert>
 #include "Quaternion.hpp"
-using namespace std;
 int main(){
     //1. String parsing test
     auto q=stoq<double>(1+2i+3j+4k);
@@ -9,16 +8,16 @@ int main(){
     assert(q.imagi()==2.0);
     assert(q.imagj()==3.0);
     assert(q.imagk()==4.0);
-    cout<<"String parsing test passed\n";
+    std::cout<<"String parsing test passed\n";
     //2. Rotation test
     auto rot=Quaternion<double>::FromAxisAngle(1.5708,0,0,1);
     auto v=rot.RotateVector(1,0,0);
-    assert(abs(v[0])<1e-6&&abs(v[1]-1)<1e-6);
-    cout<<"Rotation test passed\n";
+    assert(std::abs(v[0])<1e-6&&std::abs(v[1]-1)<1e-6);
+    std::cout<<"Rotation test passed\n";
     //3.Output format test
-    cout<<quat_tuple<<q<<endl;
-    cout<<quat_axisangle<<q<<endl;
-    cout<<q<<endl;
-    cout<<"All tests passed!";
+    std::cout<<quat_tuple<<q<<"\n";
+    std::cout<<quat_axisangle<<q<<"\n";
+    std::cout<<q<<"\n";
+    std::cout<<"All tests passed!";
     return 0;
 }
